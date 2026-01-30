88c0a1f30320b6c4193015a0217600714f9df686
jepuli124 <konsta.jalkanen.kj@gmail.com>
2026-01-30 19:36:51 +0200

    dialog works

--- scripts/dialog_handler.gd
+++ scripts/dialog_handler.gd
@@ -7,6 +7,9 @@ extends Control
 var sliceCounter = 0
 const letterWaitLength = 0.1
 
+func _ready() -> void:
+	textBox.text = dialogText
+
 func changeText(text: String) -> void:
 	if(text.length() > 0):
 		dialogText = text
@@ -16,16 +19,17 @@ func changeLargeText(text: String) -> void:
 	if(text.length() > 0):
 		dialogText = text
 	sliceCounter = 0
-	timer.timeout()
+	_on_timer_timeout()
 	
 
 func _on_timer_timeout() -> void:
 	var text = dialogText.get_slice("\n", sliceCounter)
+	print(text)
 	if(text.length() == 0):
 		textBox.text = ""
 		timer.stop()
 		return
 	sliceCounter += 1
 	textBox.text = text
-	timer.wait_time(text.length()*letterWaitLength)
+	timer.wait_time = text.length()*letterWaitLength + 1
 	timer.start()
---
+++ scripts/dialog_test.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var dialog = $DialogHandler
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	dialog.changeLargeText("welcome to the game
+	Apuva lol
+	Oletko koskaan ajatellu että hoomerin läskit kuteet on jotain mitä bard simson halluasi
+	he
+	")
