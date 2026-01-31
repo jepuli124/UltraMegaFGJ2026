93dc547d56c8eb607796026796314e9d8d1dd2ce
Dorfultariant <tunxero@hotmail.com>
2026-01-31 16:32:35 +0200

    Tested sprites

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+const GUEST_SCALE_FLOAT : float = 0.42
+const GUEST_SCALE : Vector3 = Vector3(GUEST_SCALE_FLOAT, GUEST_SCALE_FLOAT, GUEST_SCALE_FLOAT)
+
 var _generated_list_of_characters : Array 
 
 @onready var dialog_handler : Control = $DialogHandler
@@ -22,6 +25,7 @@ func _set_characters() -> void:
 	for chr in _generated_list_of_characters:
 		guests[i].dialogue_data = chr
 		guests[i].request_dialogue.connect(_on_guest_request_dialogue)
+		guests[i].scale = GUEST_SCALE
 		i += 1
 
 func _on_guest_request_dialogue(dialog: String) -> void:
