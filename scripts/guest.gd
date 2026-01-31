5095a14a142ae0c791b248b885afd9a69b408399
Dorfultariant <tunxero@hotmail.com>
2026-01-31 21:55:16 +0200

    Something happens but its not complete, we need one more

--- scripts/guest.gd
+++ scripts/guest.gd
@@ -4,8 +4,6 @@ class_name Guest
 
 signal request_dialogue(dialog : String)
 
-
-
 @export var target : Node3D = null
 @export var BodyTexture : Resource = null
 @export var MaskTexture : Resource = null
@@ -23,7 +21,6 @@ func _ready() -> void:
 	$Body.texture = BodyTexture
 	$Body/Mask.texture = MaskTexture
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(target.global_position, Vector3.UP)
@@ -32,3 +29,9 @@ func _process(delta: float) -> void:
 func interact(caller : Node3D) -> void:
 	var msg = dialogue_data["messages"][randi_range(0, len(dialogue_data["messages"]) -1)]["text"]
 	request_dialogue.emit(msg)
+
+func throw_out() -> void:
+	## TODO: Change mask to a blown head sprite and disable physicsbody
+	set_physics_process(false)
+	visible = false
+	
