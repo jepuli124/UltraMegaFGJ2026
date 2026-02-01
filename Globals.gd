fc9cc117fc5e956fe8aad12c6065204e796ccf37
jepuli124 <konsta.jalkanen.kj@gmail.com>
2026-02-01 22:06:26 +0200

    fix noices, added exit button
    All sound is now not as loud
    Escape now quits anywhere

--- Globals.gd
+++ Globals.gd
@@ -11,4 +11,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if(Input.is_key_pressed(KEY_ESCAPE)):
+		get_tree().quit()
